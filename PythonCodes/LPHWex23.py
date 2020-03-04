5c064034fd491ad0393f2e298fa695094b333af4
CodeKratos <42566864+CodeKratos@users.noreply.github.com>
2020-03-04 16:06:00 +0530

    Added excercise 25
    Ex 25- code works fine but lots of changes are needed.
               1. Add print statements to make each result visible
               2. Play more with codes as much as you can.

--- PythonCodes/LPHWex23.py
+++ PythonCodes/LPHWex23.py
@@ -1,11 +1,10 @@
 # Page 97 Excercise Strings, Bytes and Character Encoding
 
-import sys #importig system
-script, encoding, error = sys.argv #getting arguments
+import sys #importig sys module to use aya.argv
+script, encoding, error = sys.argv # sys.argv is a list that stores command line arguments
 
 def main(language_file, encoding, errors): # 2. defining main function readline() returns one line from language_file
     line = language_file.readline()  #3.
-
     if line:                                    #4. no specific condition 
         print_line(line, encoding, errors)       #5.           #calling print_line with custom parameters line, encodin and error
         return main(language_file, encoding, errors)
@@ -14,7 +13,6 @@ def print_line(line, encoding, errors):  # 6. function being executed
     next_lang = line.strip()            #7. removes all the spaces form the line
     raw_bytes = next_lang.encode(encoding, errors = errors) #8. 
     cooked_string = raw_bytes.decode(encoding, errors = errors)
-
     print(raw_bytes, "<====>", cooked_string)
 
 language = open("language.txt", encoding ='utf-8')
