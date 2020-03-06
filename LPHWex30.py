3131c7e9675647059deaae6ddcad233ffef3034e
CodeKratos <42566864+CodeKratos@users.noreply.github.com>
2020-03-06 17:23:08 +0530

    Else If excersise 30
    Else if statement done.
    We need to go faster......................

---
+++ LPHWex30.py
@@ -0,0 +1,16 @@
+#else if
+#page number 125
+
+print("how much money jeff bezoz has?")
+bezos = input("$$$")
+print("how much money does bill gates has?")
+gates = input("$$$")
+print("how much money does buffet has?")
+buffet = input("$$$")
+
+if bezos > gates:
+    print("Bezos is richer than Gates")
+elif gates > buffet:
+    print("Gates is richer than buffe and bezos")
+else:
+    print("buffet is richer than bezos and gates")
