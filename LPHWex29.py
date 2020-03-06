b4d99dd8e6bf511f0c6cb6a62e88ddea91b7abec
CodeKratos <42566864+CodeKratos@users.noreply.github.com>
2020-03-06 16:52:54 +0530

    Excercise 29 If statement
    Worked with if statement.
    Gotta go fast.....................~.' ~.' ~

---
+++ LPHWex29.py
@@ -0,0 +1,30 @@
+
+
+print("enter the number of people")
+people = input(">>>")
+print("entre the number of cats")
+cats = input("--->")
+print("eter the number of dogs")
+dogs = input(">>-->>")
+if people < cats:
+    print("Too many cats! The world is doomed")
+
+if people < dogs:
+    print("yay play day ! but we need more dogs")
+
+if people >cats:
+    print("Not so many cats! World is safe")
+
+if people >dogs:
+    print("we need more people to play with dogs")
+
+dogs += 5
+
+if people >= dogs:
+    print("we have more people but less dogs may be equal but we need more dogs")
+
+if people <= dogs:
+    print("we can play!")
+
+if people == dogs:
+    print("humans are superior in intellect but we are now equal to dogs- in number")
