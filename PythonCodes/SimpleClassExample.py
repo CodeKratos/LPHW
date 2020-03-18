09718f9c72a2193f82edbe01eae55f0925559d06
Akshay Anand Shukla <42566864+CodeKratos@users.noreply.github.com>
2020-03-18 15:07:31 +0530

    Classe
    Simpler example

---
+++ PythonCodes/SimpleClassExample.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def printu(self):
+        print("Hello my name is:" +self.name)
+
+
+p1 = Person("Jhon", 36) # p1 is a object whi
+p1.printu()
