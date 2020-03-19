a73b64eef018f7f1599c112ba4ddc6adf2665ee6
Akshay Anand Shukla <42566864+CodeKratos@users.noreply.github.com>
2020-03-19 12:49:18 +0530

    Updated
    Made a new class and used formatted string in the printing.

--- PythonCodes/SimpleClassExample.py
+++ PythonCodes/SimpleClassExample.py
@@ -6,6 +6,16 @@ class Person:
     def printu(self):
         print("Hello my name is:" +self.name)
 
+class Games:
+    def __init__(self,game,protagonist):
+        self.game = game
+        self.protagonist = protagonist
+    def message(self):
+        print(f" In {self.game}, {self.protagonist} is my favourit character")
 
-p1 = Person("Jhon", 36) # p1 is a object whi
+        
+p1 = Person("Akshay", 25) # p1 is a object whi
 p1.printu()
+
+g = Games("God of War","Kratos")
+g.message()
